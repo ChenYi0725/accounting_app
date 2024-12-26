1e0a6ec27b4e74c983d38fe64463066092739407
eason <eason.jshouse0725@gmail.com>
2024-12-27 06:31:02 +0800

    add bar code

---
+++ lib/model/bar_code.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:barcode_widget/barcode_widget.dart';
+import 'package:accounting_app/Database/database_helper.dart';
+
+class BarCode extends StatelessWidget {
+  DatabaseHelper databaseHelper = DatabaseHelper();
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder<Map<String, dynamic>?>(
+      future: databaseHelper.fetchVehicle(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return CircularProgressIndicator(); // 顯示載入中
+        } else if (snapshot.hasError) {
+          return Text('Error: ${snapshot.error}');
+        } else if (snapshot.hasData) {
+          final vehicleData = snapshot.data!;
+          final vehicleNumber = vehicleData['number'];
+
+          return Column(
+            children: [
+              LayoutBuilder(
+                builder: (context, constraints) {
+                  final double screenWidth = constraints.maxWidth;
+                  return BarcodeWidget(
+                    data: vehicleNumber,
+                    barcode: Barcode.code128(), // or other barcode type
+                    width: screenWidth,
+                    height: screenWidth / 4,
+                    color: Colors.black,
+                    backgroundColor: Colors.white,
+                  );
+                },
+              ),
+            ],
+          );
+        } else {
+          return Text('無載具資料');
+        }
+      },
+    );
+  }
+}
--- lib/model/main_page_drawer.dart
+++ lib/model/main_page_drawer.dart
@@ -1,7 +1,36 @@
+import 'package:accounting_app/Database/database_helper.dart';
 import 'package:flutter/material.dart';
 
-class MainPageDrawer extends StatelessWidget {
-  const MainPageDrawer({super.key});
+class MainPageDrawer extends StatefulWidget {
+  final VoidCallback updateMainPage;
+  MainPageDrawer({super.key, required this.updateMainPage});
+
+  @override
+  _MainPageDrawerState createState() => _MainPageDrawerState();
+}
+
+class _MainPageDrawerState extends State<MainPageDrawer> {
+  DatabaseHelper databaseHelper = DatabaseHelper();
+  String? vehicleNumber;
+  TextEditingController _controller = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+    _loadCurrentVehicle();
+  }
+
+  void _loadCurrentVehicle() async {
+    Map<String, dynamic>? vehicle = await databaseHelper.fetchVehicle();
+    setState(() {
+      vehicleNumber = vehicle != null ? vehicle['number'] : null;
+    });
+  }
+
+  void _updateVehicleNumber(String number) async {
+    await databaseHelper.updateVehicle(number);
+    _loadCurrentVehicle();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -22,9 +51,47 @@ class MainPageDrawer extends StatelessWidget {
             ),
           ),
           ListTile(
-            title: Text('選項 1'),
+            title: const Text('設定載具條碼'),
             onTap: () {
-              // 點擊選項後的操作
+              showDialog(
+                context: context,
+                builder: (context) {
+                  return AlertDialog(
+                    title: Text('更新載具號碼'),
+                    content: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        Text(
+                          vehicleNumber != null
+                              ? '目前的載具號碼：$vehicleNumber'
+                              : '目前沒有載具',
+                        ),
+                        TextField(
+                          controller: _controller,
+                          decoration: InputDecoration(hintText: '輸入新載具號碼'),
+                        ),
+                      ],
+                    ),
+                    actions: [
+                      TextButton(
+                        onPressed: () {
+                          Navigator.of(context).pop();
+                        },
+                        child: Text('取消'),
+                      ),
+                      TextButton(
+                        onPressed: () {
+                          _updateVehicleNumber(_controller.text);
+
+                          Navigator.of(context).pop();
+                          widget.updateMainPage();
+                        },
+                        child: Text('確認'),
+                      ),
+                    ],
+                  );
+                },
+              );
             },
           ),
           ListTile(
